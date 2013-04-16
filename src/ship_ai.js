31e2dae0a13806a6e6d74e99b72d81cc99ce262f
Andrew Kelley <superjoe30@gmail.com>
2013-04-16 00:42:04 -0400

    AI is a little smarter

--- src/ship_ai.js
+++ src/ship_ai.js
@@ -15,15 +15,41 @@ function ShipAi(ship) {
 }
 
 ShipAi.prototype.update = function (dt, dx, state) {
-  if (! this.target) this.chooseTarget();
-  this.ship.shootInput = this.goodShotAtTarget() ? 1 : 0;
+  if (! this.target) this.chooseTarget(state);
+  if (! this.target) {
+    this.ship.shootInput = 0;
+    return;
+  }
+
+  var targetAngle = this.target.ship.pos.minus(this.ship.pos).angle();
+  var delta = targetAngle - this.ship.rotation;
+  var goodShot = Math.abs(delta) < Math.PI / 10;
+  var reallyGoodShot = Math.abs(delta) < Math.PI / 20;
+
+  // shoot at target
+  this.ship.shootInput = goodShot ? 1 : 0;
+
+  // aim at target
+  this.ship.rotateInput = reallyGoodShot ? 0 : sign(delta);
 }
 
-ShipAi.prototype.chooseTarget = function() {
-  // TODO
+ShipAi.prototype.chooseTarget = function(state) {
+  for (var id in state.aiObjects) {
+    var ai = state.aiObjects[id];
+    if (ai.ship.team !== this.ship.team) {
+      this.target = ai;
+      return;
+    }
+  }
+  this.target = null;
 };
 
-ShipAi.prototype.goodShotAtTarget = function() {
-  // TODO
-  return true;
-};
+function sign(x) {
+  if (x > 0) {
+    return 1;
+  } else if (x < 0) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
